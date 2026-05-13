d5c03383223c998757682a331f082c65fe4cdb33
alexgduarte <alexduartegomescouto@gmail.com>
2026-05-13 15:47:12 +0100

    add OpenCL keyword identifier regression test

---
+++ src/test/java/com/aparapi/codegen/test/OpenCLKeywordIdentifier.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2016 - 2018 Syncleus, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aparapi.codegen.test;
+
+public class OpenCLKeywordIdentifier {
+
+    int[] global = new int[1];
+
+    int[] result = new int[1];
+
+    int compute(int kernel) {
+        int local = kernel + 1;
+        return local;
+    }
+
+    public void run() {
+        int id = 0;
+        result[id] = compute(global[id]);
+    }
+}
